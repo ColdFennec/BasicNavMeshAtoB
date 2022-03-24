dfa75c25af9bf34017453a2b7e0ebf6426092d99
ColdFennec <xColdLight@Outlook.com>
2022-03-24 14:55:18 +0100

    Add more options to go target, player or distance range

--- Assets/AINavMesh.cs
+++ Assets/AINavMesh.cs
@@ -6,17 +6,64 @@ using UnityEngine.AI;
 public class AINavMesh : MonoBehaviour
 {
 
+
+    public bool setFollowFlag = false;
+    public bool setFollowPlayer = false;
+    public bool setFollowPlayerByDistance = false;
+
+    [Range(5, 10)]
+    public int setDistanceDetect;
+    
+    private Transform targetPosition;
+
     [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private Transform movePositionPlayer;
+
     private NavMeshAgent navMeshAgent;
 
 
     private void Awake()
-    {
+    {   
+
+
         navMeshAgent = GetComponent<NavMeshAgent>();
+
+        if (setFollowFlag)
+        {
+            targetPosition = movePositionTransform;
+        } 
+        else if (setFollowPlayer)
+        {
+            targetPosition = movePositionPlayer;
+        } 
+        else if (setFollowPlayerByDistance)
+        {
+            targetPosition = movePositionPlayer;
+            SphereCollider rangeSphere = GetComponent<SphereCollider>();
+            rangeSphere.radius = setDistanceDetect;
+        }
+
     }
 
     private void Update()
+    {   if (setFollowFlag || setFollowPlayer)
+        {
+            navMeshAgent.destination = targetPosition.position;
+        } 
+        else if (setFollowPlayerByDistance)
+        {   
+            DistanceTarget();
+        }
+
+    }
+
+
+    private void DistanceTarget()
     {
-        navMeshAgent.destination = movePositionTransform.position;
+        float distance = Vector3.Distance(targetPosition.position, transform.position);
+        if (distance < setDistanceDetect)
+        {
+            navMeshAgent.destination = targetPosition.position;
+        }
     }
 }
